46e7216d32946f6d35b0f394fc82c3cecb72e1e5
Anurag Bahl <abahl@salesforce.com>
2022-10-19 17:40:28 -0700

    added a comment to test jenkins build

--- src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * test-jenkins
  */
 
 package org.springframework.samples.petclinic;
@@ -29,7 +30,6 @@ class PetclinicIntegrationTests {
 
 	@Test
 	void testFindAll() throws Exception {
-                // added  a comment
 		vets.findAll();
 		vets.findAll(); // served from cache
 	}
