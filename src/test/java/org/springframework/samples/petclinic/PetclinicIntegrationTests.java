16eea3f47db52ea489f5176e16f1f1d0e768607d
Anurag Bahl <abahl@salesforce.com>
2022-10-19 17:47:55 -0700

    added a comment to test jenkins build

--- src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may need obtain a copy of the License at
  *
  *      https://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * test-jenkins
  */
 
 package org.springframework.samples.petclinic;
